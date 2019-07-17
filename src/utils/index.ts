08b5b9560adc5127710ef82e2dd7c082986f2f32
dannysofftie <dankim761@gmail.com>
2019-07-17 15:21:08 +0300

    Add validation to csv parser

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ import { IStatusCodesInterface, statuscodes } from './Statuscodes';
 import { ICompileTemplate, compileEjs } from './Template';
 import { IJWTToken, JWTToken } from './Token';
 import { IEmail, sendEmail } from '../libraries/Email';
-import { ICsvparser, parse } from './Csvparser';
+import { ICsvparser, parseCsv } from './Csvparser';
 import { IResolveLocals, resolve } from './Resolvelocals';
 import { IPaginate, paginate } from './Paginate';
 import { ISheetbuilder, buildSheet } from './Sheetbuilder';
@@ -15,7 +15,7 @@ import { ISheetbuilder, buildSheet } from './Sheetbuilder';
 export interface IUtilities extends IObjectdiff, IUploader, IStatusCodesInterface, ICompileTemplate, IJWTToken, IEmail, ICsvparser, IResolveLocals, IPaginate, ISheetbuilder {}
 
 export default fp((app: FastifyInstance, opts: {}, done: (err?: Error) => void) => {
-    app.decorate('utils', { compareObjects, uploader, statuscodes, compileEjs, ...JWTToken, sendEmail, extractFilepath, parse, resolve, paginate, buildSheet });
+    app.decorate('utils', { compareObjects, uploader, statuscodes, compileEjs, ...JWTToken, sendEmail, extractFilepath, parseCsv, resolve, paginate, buildSheet });
 
     // pass execution to the next middleware in fastify instance
     done();
