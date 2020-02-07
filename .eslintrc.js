d80f723b4f98c286a5eb531443170008aab8b25d
dannysofftie <dankim761@gmail.com>
2020-02-07 11:46:34 +0300

    Add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/no-use-before-define': 'off',
         '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
     },
 };
