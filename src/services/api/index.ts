9dc111b863b85f6bd37482dd82f81885b9ad46d9
dannysofftie <dankim761@gmail.com>
2020-05-05 08:18:15 +0300

    Move routes to services/

---
+++ src/services/api/index.ts
@@ -0,0 +1,163 @@
+import { FastifyInstance } from 'fastify';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import { protectUserRoute, protectAuthorizedUser } from '../../middlewares/Authentication';
+import Products from '../../controllers/Products';
+
+export default (app: FastifyInstance<Server, IncomingMessage, ServerResponse>, opts: { prefix: string }, next: (err?: Error) => void) => {
+    app.get(
+        '/get-route',
+        {
+            preHandler: protectAuthorizedUser,
+            schema: {
+                description: 'Description of this get route.',
+                tags: ['api'],
+                response: {
+                    200: {
+                        description: 'Successful response',
+                        type: 'object',
+                        properties: {
+                            email: { type: 'string', description: 'Sample field referring to an email field' },
+                            item: { type: 'string', enum: ['enum1', 'enum2', 'enum3', 'enum4', 'enum5'], description: 'This item field will only accept defined entries in the enum definition.' },
+                        },
+                    },
+                },
+                summary: 'Get request',
+                security: [
+                    {
+                        apiKey: [],
+                    },
+                ],
+            },
+        },
+        async (req, res) => new Products(app, req, res).getOneItem()
+    );
+
+    app.get(
+        '/path-with-param/:id',
+        {
+            preHandler: protectAuthorizedUser,
+            schema: {
+                tags: ['api'],
+                description: 'Protected path with route parameter',
+                response: {
+                    200: {
+                        type: 'object',
+                        properties: {
+                            id: { type: 'string' },
+                            name: { type: 'string' },
+                            // add other fields here
+                        },
+                    },
+                },
+                params: {
+                    type: 'object',
+                    properties: {
+                        id: { type: 'string', description: 'Id of the object being referenced' },
+                    },
+                },
+                summary: 'Get object information using id',
+                security: [
+                    {
+                        apiKey: [],
+                    },
+                ],
+            },
+        },
+        async (req, res) => await new Products(app, req, res).findOneAndUpdate()
+    );
+
+    app.post(
+        '/example-post-route',
+        {
+            preHandler: protectUserRoute,
+            schema: {
+                tags: ['api'],
+                description: 'Create a resource, with all the fields marked with * as required.',
+                response: {
+                    ...app.plugins.statuscodes,
+                },
+                body: {
+                    type: 'object',
+                    properties: {
+                        title: { type: 'string', description: 'Sample entry title, e.g Resource title.' },
+                        description: { type: 'string', description: 'Resource description' },
+                    },
+                    required: ['title'],
+                },
+                summary: 'Create a new resource',
+                security: [
+                    {
+                        apiKey: [],
+                    },
+                ],
+            },
+        },
+        async (req, res) => await new Products(app, req, res).addNewEntry()
+    );
+
+    app.put(
+        '/put-route-with-param/:id',
+        {
+            preHandler: protectUserRoute,
+            schema: {
+                tags: ['api'],
+                description: 'Update route as put request.',
+                response: {
+                    ...app.plugins.statuscodes,
+                },
+                body: {
+                    type: 'object',
+                    properties: {
+                        title: { type: 'string', description: 'Description of this title field.' },
+                        description: { type: 'string', description: 'Description of description field' },
+                    },
+                },
+                params: {
+                    type: 'object',
+                    properties: {
+                        id: { type: 'string', description: 'Id of the object being referenced' },
+                    },
+                },
+                summary: 'Update an object',
+                security: [
+                    {
+                        apiKey: [],
+                    },
+                ],
+            },
+        },
+        async (req, res) => await new Products(app, req, res).findOneAndUpdate()
+    );
+
+    app.delete(
+        '/delete-route/:id',
+        {
+            preHandler: protectUserRoute,
+            schema: {
+                description: 'Remove an object.',
+                tags: ['api'],
+                params: {
+                    type: 'object',
+                    properties: {
+                        id: { type: 'string', description: 'Id of the object being referenced' },
+                    },
+                },
+                response: {
+                    ...app.plugins.statuscodes,
+                },
+                summary: 'Remove an item from a collection',
+                security: [
+                    {
+                        apiKey: [],
+                    },
+                ],
+            },
+        },
+        async (req, res) => await new Products(app, req, res).getOneItem()
+    );
+
+    // pass to the next middleware
+    next();
+};
+
+exports.autoPrefix = '/api';
